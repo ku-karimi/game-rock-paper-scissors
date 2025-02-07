6891fab7dbc5bda567682170c75a395ff9762fb5
ku-karimi <karimi.z8346@gmail.com>
2025-02-07 14:48:04 +0330

    add js file

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const body = document.querySelector("body");
-const scoreNumber = document.getElementById("scoreNumber");
+let scoreNumber = document.getElementById("scoreNumber");
 const btnRules = document.getElementById("btn");
 const resultDiv = document.getElementById("resultDiv");
 const imgRuls = ["./img/icon-rock.svg" , "./img/icon-scissors.svg" , "./img/icon-paper.svg"];
 const imgSelectYou = document.getElementById("you");
 const imgRandom = document.getElementById("but");
 const rulsDiv = document.getElementById("rulsDiv");
+const panelChose = document.getElementById("panelChose");
+const playAgain = document.getElementsByClassName("play-again");
+let resultP = document.getElementById("result")
 let open = false ;
+let scour = 0 ;
 
-// btn add click event
+// btn add click event to explain game
 
 function discription() {
     if(!open){
@@ -23,3 +27,56 @@ function discription() {
 }
 
 btnRules.addEventListener("click" , discription)
+
+// start game whit chose
+
+function win(scour,resultP) {
+    resultP.innerHTML = "YOU WIN" ;
+    resultP.style.color = "#6E8E59" ;
+    scour++ ;
+    scoreNumber.innerHTML = scour ;
+}
+
+function lose(resultP) {
+    resultP.innerHTML = "YOU LOSE" ;
+    resultP.style.color = "#780C28" ;
+}
+
+function game(event) {
+    panelChose.style.display = "none" ;
+    resultDiv.style.display = "block" ;
+    let srcImg = event.target.src ;
+    imgSelectYou.src = srcImg ;
+    let i = Math.floor(Math.random() * imgRuls.length);
+    let imgSrcRandom = imgRuls[i];
+    imgRandom.src = imgSrcRandom ;
+    btnRules.style.display = "none" ;
+
+    if (event.target.src == imgSrcRandom) {
+        resultP.innerHTML = "EQUAL" ;
+        resultP.style.color = "#5C7285"
+    }else if (event.target.src == imgRuls[0] && imgSrcRandom == imgRuls[1]) {
+        win() ;
+    }else if (event.target.src == imgRuls[0] && imgSrcRandom == imgRuls[2]) {
+        lose() ;
+    }else if (event.target.src == imgRuls[2] && imgSrcRandom == imgRuls[1]) {
+        lose() ;
+    }else if (event.target.src == imgRuls[2] && imgSrcRandom == imgRuls[0]) {
+        win() ;
+    }else if(event.target.src == imgRuls[1] && imgSrcRandom ==  imgRuls[2]){
+        win() ;
+    }else if(event.target.src == imgRuls[1] && imgSrcRandom ==  imgRuls[0]){
+        lose() ;
+    }
+}
+
+// play again btn
+
+function play() {
+    panelChose.style.display = "block" ;
+    resultDiv.style.display = "none" ;
+    btnRules.style.display = "block" ;
+}
+
+playAgain[0].addEventListener("click" , play)
+
